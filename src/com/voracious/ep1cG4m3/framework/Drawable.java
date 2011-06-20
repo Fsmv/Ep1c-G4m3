965736886615cdbe3d5e867e6e1ebe10267c6fe0
FsmvDA@gmail.com <FsmvDA@gmail.com@8f0d591c-cda0-088a-324b-2236c296bc3a>
2011-06-20 22:17:54 +0000

    + Finished Drawable
    git-svn-id: https://ep1c-g4m3.googlecode.com/svn/trunk@9 8f0d591c-cda0-088a-324b-2236c296bc3a

--- src/com/voracious/ep1cG4m3/framework/Drawable.java
+++ src/com/voracious/ep1cG4m3/framework/Drawable.java
@@ -1,5 +1,62 @@
 package com.voracious.ep1cG4m3.framework;
 
-public class Drawable {
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
 
+import com.voracious.ep1cG4m3.utils.Location;
+
+public class Drawable {
+	private Image myImage;
+	private boolean isVisible;
+	private Location myLocation;
+	
+	public Drawable(){
+		myImage = (Image) null;
+		isVisible = false;
+		myLocation = new Location();
+	}
+	
+	public Drawable(Image image){
+		myImage = image;
+		isVisible = false;
+		myLocation = new Location();
+	}
+	
+	public Drawable(Image image, boolean visible){
+		myImage = image;
+		isVisible = visible;
+		myLocation = new Location();
+	}
+	
+	public Drawable(Image image, boolean visible, Location location){
+		myImage = image;
+		isVisible = visible;
+		myLocation = location;
+	}
+	
+	public void draw(Graphics2D page){
+		if(isVisible)
+			page.drawImage(myImage, (int)myLocation.getX(), (int)myLocation.getY(), (ImageObserver)null);
+	}
+	
+	public void setImage(Image image){
+		myImage = image;
+	}
+	
+	public void setVisible(boolean visible){
+		isVisible = visible;
+	}
+	
+	public Image getImage(){
+		return myImage;
+	}
+	
+	public Location getLocation(){
+		return myLocation;
+	}
+	
+	public boolean isVisible(){
+		return isVisible;
+	}
 }
