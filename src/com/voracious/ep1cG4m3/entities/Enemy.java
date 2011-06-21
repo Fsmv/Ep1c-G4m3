ab28c1e915a34b531fbc46c422b9138fd8904ef8
Fsmv <fsmvda@gmail.com>
2011-06-21 00:22:58 +0000

    + Added GNU v3 comment in all files + Finished all but hitTest in Tile - Renamed Location to Point (Location is used in many other librarys, I'm avoiding conflicts)

--- src/com/voracious/ep1cG4m3/entities/Enemy.java
+++ src/com/voracious/ep1cG4m3/entities/Enemy.java
@@ -1,5 +1,24 @@
 package com.voracious.ep1cG4m3.entities;
 
+/*  
+ *  Ep1c G4m3 -- A parody platformer
+ * 
+ *  Copyright (C) 2011  Voracious Softworks
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 import com.voracious.ep1cG4m3.framework.Entity;
 
 public class Enemy extends Entity {
--- src/com/voracious/ep1cG4m3/framework/Drawable.java
+++ src/com/voracious/ep1cG4m3/framework/Drawable.java
@@ -1,38 +1,57 @@
 package com.voracious.ep1cG4m3.framework;
 
+/*  
+ *  Ep1c G4m3 -- A parody platformer
+ * 
+ *  Copyright (C) 2011  Voracious Softworks
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
 import java.awt.Graphics2D;
-import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 
-import com.voracious.ep1cG4m3.utils.Location;
+import com.voracious.ep1cG4m3.utils.Point;
 
 public class Drawable {
-	private Image myImage;
+	private BufferedImage myImage;
 	private boolean isVisible;
-	private Location myLocation;
+	private Point myLocation;
 	
 	public Drawable(){
-		myImage = (Image) null;
+		myImage = (BufferedImage) null;
 		isVisible = false;
-		myLocation = new Location();
+		myLocation = new Point();
 	}
 	
-	public Drawable(Image image){
+	public Drawable(BufferedImage image){
 		myImage = image;
 		isVisible = false;
-		myLocation = new Location();
+		myLocation = new Point();
 	}
 	
-	public Drawable(Image image, boolean visible){
+	public Drawable(BufferedImage image, boolean visible){
 		myImage = image;
 		isVisible = visible;
-		myLocation = new Location();
+		myLocation = new Point();
 	}
 	
-	public Drawable(Image image, boolean visible, Location location){
+	public Drawable(BufferedImage image, boolean visible, Point point){
 		myImage = image;
 		isVisible = visible;
-		myLocation = location;
+		myLocation = point;
 	}
 	
 	public void draw(Graphics2D page){
@@ -40,7 +59,7 @@ public class Drawable {
 			page.drawImage(myImage, (int)myLocation.getX(), (int)myLocation.getY(), (ImageObserver)null);
 	}
 	
-	public void setImage(Image image){
+	public void setImage(BufferedImage image){
 		myImage = image;
 	}
 	
@@ -48,11 +67,11 @@ public class Drawable {
 		isVisible = visible;
 	}
 	
-	public Image getImage(){
+	public BufferedImage getImage(){
 		return myImage;
 	}
 	
-	public Location getLocation(){
+	public Point getLocation(){
 		return myLocation;
 	}
 	
